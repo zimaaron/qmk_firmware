f0457ada1d7ef9e3bc706a4d820f06af04f48e08
Aaron Osgood-Zimmerman <azimmer@uw.edu>
2019-12-14 15:47:20 -0800

    adding current atreus62 files

---
+++ keyboards/atreus62/keymaps/whyfarer/config.h
@@ -0,0 +1,17 @@
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM 200
+// #define RETRO_TAPPING
+
+// AUTOSHIFT
+// shut off bc doesnt play nice with MT (used on home row)
+// also put following line in rules.mk:
+// AUTO_SHIFT_ENABLE = yes
+
+//#define NO_AUTO_SHIFT_SPECIAL
+//#define NO_AUTO_SHIFT_NUMERIC
+//#define AUTO_SHIFT_TIMEOUT 120
+
+
+
+
